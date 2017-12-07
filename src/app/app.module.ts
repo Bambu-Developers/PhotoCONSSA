dcfc606fe5fa7c9d9e7307feb19bb86860de94ca
Luis Rafael Rivera <30272643+RafaelBambu@users.noreply.github.com>
2017-12-06 20:41:33 -0600

    Se hizo la primer pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './components/landing/landing.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LandingComponent}
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LandingComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
