6b672f955e04c821364a53f9ee8daa5287299b9a
Luis Rafael Rivera <30272643+RafaelBambu@users.noreply.github.com>
2017-12-18 13:10:47 -0600

    Falta configurar firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {
